227fd16f075b80ba479674965ba8ff807f2f167a
Maxim <endurancemayer@yandex.ru>
2016-05-18 20:15:12 +0500

    Abstract part for Ships

---
+++ Invaders/Ship.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using System.Drawing;
+
+namespace Invaders
+{
+   abstract class Ship
+    {
+        public Point Location { get; protected set; }
+
+        public Size Size { get; private set; }
+
+        public Rectangle Area { get { return new Rectangle(Location, Size); } }
+
+        public Ship(Point location, Size size)
+        {
+            Location = location;
+            Size = size;
+        }
+
+        public abstract void Move(Direction direction);
+   
+    }
+}
